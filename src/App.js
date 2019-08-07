26f33fd58937ac89a6848ec4a2b7acbcbf82b899
m-zaremba <zaremba_michal@wp.pl>
2019-08-07 22:12:41 +0200

    Initial file structure commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class App extends React.Component {
+
+   render(){
+     return (
+       <div className='App'>
+        Hello World!
+       </div>
+     )
+   }
+ }
+
+export default App;
